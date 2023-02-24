1747faccb5b53bf17b52c2a37a6a58f7c91369aa
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-02-24 21:59:48 +0530

    Modified

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-$("a").attr("href","http://www.yahoo.com");
+for(var i=0;i<5;i++)
+{
+  document.querySelectorAll("button")[i].addEventListener("click",function(){
+    document.querySelector("h1").style.color="purple";
+  })
+}
+
+$("button").click(function(){
+  $("h1").css("color","purple");
+})
