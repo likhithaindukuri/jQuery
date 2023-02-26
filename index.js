6c1570d3b2aed310de2ba361e5d416b7ce17cc56
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-02-26 10:17:16 +0530

    Modified

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-for(var i=0;i<5;i++)
-{
-  document.querySelectorAll("button")[i].addEventListener("click",function(){
-    document.querySelector("h1").style.color="purple";
-  })
-}
-
 $("button").click(function(){
   $("h1").css("color","purple");
 })
